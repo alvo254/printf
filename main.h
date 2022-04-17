b60aab99d671b17e7fcddf46d2985b954d0d7dc0
alvo254 <alvinkhamron@gmail.com>
2022-04-17 17:59:00 +0300

    correction

--- main.h
+++ main.h
@@ -1,41 +1,59 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef PRINTF_H
+#define PRINTF_H
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <limits.h>
+#include <float.h>
 #include <unistd.h>
+
 /**
- * struct type - connect conversion specifiers with the correct print function
- * @print: a function pointer for the print functions
- * @identifier: the conversion specifier
+ * struct mark - sructure for format
+ * Description: type to define the behavior of specifier
+ * @opointer: first member
+ * @fpointer: second member
  */
-typedef struct type
+typedef struct mark
 {
-	char *identifier;
-	int (*print)(va_list);
-} type_t;
+	char *opointer;
+	int (*fpointer)(va_list p);
+} mark_t;
 
+int _printf(const char *format, ...);
+int specifier(int *i, const char *format, va_list list);
 int _putchar(char c);
-int (*get_function(const char *specifier))(va_list);
-int _printf(const char *, ...);
-int _strlen(char *);
-int print_rev(va_list args);
-int rot13(va_list args);
-int print_number(unsigned int n);
-int countDigits(unsigned int num);
-int countOctal(unsigned int num);
-int countBinary(unsigned int num);
-int print_b(va_list args);
-int print_o(va_list args);
-int print_d(va_list args);
-int print_x(va_list args);
-void print_lowerHex(unsigned int num, int *count);
-int print_X(va_list args);
-void print_upperHex(unsigned int num, int *count);
-int print_p(va_list args);
-int print_s(va_list args);
-int print_c(va_list args);
-int print_u(va_list args);
-int print_F(va_list args);
 
-#endif /* HOLBERTON_H */
+/**
+ * struct noncustom - non custom specifier
+ * when a flag specifier is passed to _printf()
+ * @sum: '+' character
+ * @empty: ' ' character
+ * @hash: '#' character
+ */
+typedef struct noncustom
+{
+	int sum;
+	int empty;
+	int hash;
+} noncustom_t;
+/* all calls from specifier */
+int p_c(va_list p);
+int p_s(va_list p);
+int p_p(va_list __attribute__((unused)) p);
+int p_d(va_list p);
+int p_i(va_list p);
+int p_b(va_list p);
+int p_u(va_list p);
+int p_o(va_list p);
+int p_x(va_list p);
+int p_Xu(va_list p);
+int p_Su(va_list p);
+int p_pt(va_list p);
+int p_r(va_list p);
+int p_Rt(va_list __attribute__((unused)) p);
+
+/* auxiliar functions */
+int aux_S_hex(int n);
+
+#endif
